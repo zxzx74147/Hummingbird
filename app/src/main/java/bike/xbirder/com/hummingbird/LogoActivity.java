561b8e346c4b7794498bf21ae7bbf17ad62cb69f
zhengxin <422583341@qq.com>
2015-07-06 17:15:28 +0800

    first commit

---
+++ app/src/main/java/bike/xbirder/com/hummingbird/LogoActivity.java
@@ -0,0 +1,16 @@
+package bike.xbirder.com.hummingbird;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class LogoActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+    }
+
+
+}
